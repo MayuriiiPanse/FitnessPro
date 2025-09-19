acb54961d1a6b228510620391b86b668dd4fa39c
Mayuri Panse <mayuripanse16@gmail.com>
2025-09-19 12:49:51 +0530

    on branch main

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./App";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter basename="/FitnessPro">
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
